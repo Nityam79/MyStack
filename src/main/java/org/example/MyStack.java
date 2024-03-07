13b821e2ea8cce07839ffbe33cd1753291cea1a7
nityamgoswami <goswaminityam9@gmail.com>
2024-03-06 20:12:13 -0500

    added delegate variable and initialize it in the constructor

--- src/main/java/org/example/MyStack.java
+++ src/main/java/org/example/MyStack.java
@@ -3,8 +3,12 @@ package org.example;
 import java.util.ArrayList;
 import java.util.EmptyStackException;
 
-
 public class MyStack<E> extends ArrayList<E> {
+    private ArrayList<E> delegate;
+
+    public MyStack() {
+         delegate = this;
+    }
 
     public void push(E e) {
         add(e);
@@ -13,8 +17,9 @@ public class MyStack<E> extends ArrayList<E> {
     public E pop() {
         if (isEmpty()) throw new EmptyStackException();
         E e = get(size() - 1);
-        remove(size() -1 );
+       remove(size() - 1);
         return e;
     }
+
 
 }
